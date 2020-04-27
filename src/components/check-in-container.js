6ac713b71783f97d32e50c12ac340abc2aa67b15
Anthonyg6 <anthonygallegos6@gmail.com>
2020-04-26 20:23:20 -0600

    had to reupdate routes

--- src/components/check-in-container.js
+++ src/components/check-in-container.js
@@ -20,7 +20,7 @@ export default class CheckInContainer extends Component {
 
   getDriverCheckIn() {
     axios
-      .get("http://localhost:4000/check-in")
+      .get("https://driver-check-in-server.herokuapp.com/check-in")
       .then((response) => {
         this.setState({
           checkInInfo: response.data,
@@ -49,7 +49,7 @@ export default class CheckInContainer extends Component {
   updateCheckOutTime(_id) {
     event.preventDefault();
     axios
-      .put(`driver-check-in-server/check-in/${_id}`, {
+      .put(`https://driver-check-in-server.herokuapp.com/check-in/${_id}`, {
         checkOutTime: moment().format("LT"),
         isCheckedIn: false,
       })
@@ -70,7 +70,7 @@ export default class CheckInContainer extends Component {
   removeEntry(_id) {
     event.preventDefault();
     axios
-      .delete(`driver-check-in-server.herokuapp.com/check-in/${_id}`)
+      .delete(`https://driver-check-in-server.herokuapp.com/check-in/${_id}`)
       .then(console.log("Driver removed!"))
       .catch((error) => {
         console.log(error);
