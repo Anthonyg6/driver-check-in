86fee8169d6856c905aaf8f9d5e2d5feb1317c68
Anthonyg6 <anthonygallegos6@gmail.com>
2020-05-19 19:51:48 -0600

    changed the api call back functions time and still trying to fix lining up issue

--- src/components/check-in-container.js
+++ src/components/check-in-container.js
@@ -85,7 +85,7 @@ export default class CheckInContainer extends Component {
   }
 
   updateState() {
-    this.checkinInterval = setInterval(this.getDriverCheckIn, 2000);
+    this.checkinInterval = setInterval(this.getDriverCheckIn, 5000);
   }
 
   componentDidMount() {
