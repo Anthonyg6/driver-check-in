6064c3ac97b1d75640b931a820263bb77f81207c
Anthonyg6 <anthonygallegos6@gmail.com>
2020-06-09 20:37:36 -0600

    made another freaking change

--- src/components/check-in-form.js
+++ src/components/check-in-form.js
@@ -24,7 +24,60 @@ export default class CheckIn extends Component {
   }
 
   touchEvent(e) {
-    alert("i was clicked yo!!!!");
+    event.preventDefault();
+    axios({
+      method: "POST",
+      url: "https://driver-check-in-server.herokuapp.com/check-in",
+      headers: { "content-type": "application/json" },
+      data: {
+        _id: this.state._id,
+        date: this.state.date,
+        driverName: this.state.driverName,
+        carrier: this.state.carrier,
+        deliveryType: this.state.deliveryType,
+        truckType: this.state.truckType,
+        checkInTime: this.state.checkInTime,
+        checkOutTime: this.state.checkOutTime,
+        isCheckedIn: true,
+        done: false,
+      },
+    })
+      .then((data) => {
+        this.setState({
+          _ids: [...this.state._ids, data.data],
+          dates: [...this.state.dates, data.data],
+          driverNames: [...this.state.driverName, data.data],
+          carriers: [...this.state.carriers, data.data],
+          deliveryTypes: [...this.state.deliveryTypes, data.data],
+          truckTypes: [...this.state.truckTypes, data.data],
+          checkInTimes: [...this.state.checkInTimes, data.data],
+          checkOutTimes: [...this.state.checkOutTimes, data.data],
+          isCheckedIn: [...this.state.isCheckedIn, data.data],
+          _id: "",
+          date: "",
+          driverName: "",
+          carrier: "",
+          deliveryType: "",
+          truckType: "",
+          checkInTime: "",
+          checkOutTime: "",
+          isCheckedIn: true,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    this.setState({
+      _id: "",
+      date: moment().format("MMM DD YYYY"),
+      driverName: "",
+      carrier: "",
+      deliveryType: "Delivery",
+      truckType: "LTL",
+      checkInTime: moment().format("LT"),
+      checkOutTime: "",
+      isCheckedIn: false,
+    });
   }
 
   handleSubmit(e) {
