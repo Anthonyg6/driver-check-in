95e06fb9a07d9fa734aa2c089cdb517d5980beb6
Anthonyg6 <anthonygallegos6@gmail.com>
2020-06-19 21:28:55 -0600

    fixed styles for form button submit

--- src/components/check-in-form.js
+++ src/components/check-in-form.js
@@ -25,60 +25,7 @@ export default class CheckIn extends Component {
 
   touchEvent(e) {
     // event.preventDefault();
-    axios({
-      method: "POST",
-      url: "https://driver-check-in-server.herokuapp.com/check-in",
-      headers: { "content-type": "application/json" },
-      data: {
-        _id: this.state._id,
-        date: this.state.date,
-        driverName: this.state.driverName,
-        carrier: this.state.carrier,
-        deliveryType: this.state.deliveryType,
-        truckType: this.state.truckType,
-        checkInTime: this.state.checkInTime,
-        checkOutTime: this.state.checkOutTime,
-        isCheckedIn: true,
-        done: false,
-      },
-    })
-      .then((data) => {
-        this.setState({
-          _ids: [...this.state._ids, data.data],
-          dates: [...this.state.dates, data.data],
-          driverNames: [...this.state.driverName, data.data],
-          carriers: [...this.state.carriers, data.data],
-          deliveryTypes: [...this.state.deliveryTypes, data.data],
-          truckTypes: [...this.state.truckTypes, data.data],
-          checkInTimes: [...this.state.checkInTimes, data.data],
-          checkOutTimes: [...this.state.checkOutTimes, data.data],
-          isCheckedIn: [...this.state.isCheckedIn, data.data],
-          _id: "",
-          date: "",
-          driverName: "",
-          carrier: "",
-          deliveryType: "",
-          truckType: "",
-          checkInTime: "",
-          checkOutTime: "",
-          isCheckedIn: true,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    this.setState({
-      _id: "",
-      date: moment().format("MMM DD YYYY"),
-      driverName: "",
-      carrier: "",
-      deliveryType: "Delivery",
-      truckType: "LTL",
-      checkInTime: moment().format("LT"),
-      checkOutTime: "",
-      isCheckedIn: false,
-    });
-    alert("Driver has been submitted");
+    console.log("from submit");
   }
 
   handleSubmit(e) {
@@ -185,6 +132,7 @@ export default class CheckIn extends Component {
           </select>
           <input
             type="button"
+            className="form-btn"
             value="CheckIn"
             onClick={this.handleSubmit}
             onTouchEnd={this.touchEvent}
